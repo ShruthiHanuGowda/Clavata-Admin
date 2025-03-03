de7f4c71a97d1d02d73d4f6c73d176ab8dd32db1
parth shahu <parth@d.energy>
2025-03-03 14:46:18 +0530

    dashboard

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -15,7 +15,7 @@ const MaintenanceComingSoon = Loadable(lazy(() => import('pages/maintenance/comi
 const AppContactUS = Loadable(lazy(() => import('pages/contact-us')));
 // render - sample page
 
-const Dashboard = Loadable(lazy(() => import('pages/extra-pages/Dashboard')));
+const Dashboard = Loadable(lazy(() => import('pages/dashboard')));
 const User = Loadable(lazy(() => import('pages/user/user')));
 const Companies = Loadable(lazy(() => import('pages/companies/companies')));
 const Registries = Loadable(lazy(() => import('pages/registries/registries')));
