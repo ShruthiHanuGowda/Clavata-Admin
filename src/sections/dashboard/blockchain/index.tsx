39a717e99032a625c80e692e2d1a730d074976ce
parth shahu <parth@d.energy>
2025-03-24 11:28:52 +0530

    Add Denergy Testnet API integration for blockchain stats
    Introduce functions to fetch stats and chart data from the Denergy Testnet API. Update the Dashboard and Blockchain components to display real-time statistics by utilizing data from the API. Added environment variable for the API base URL configuration.

--- src/sections/dashboard/blockchain/index.tsx
+++ src/sections/dashboard/blockchain/index.tsx
@@ -7,7 +7,17 @@ import MainCard from 'components/MainCard';
 import AnalyticCard from 'components/dashboard/AnalyticCard';
 import LineChartCard from 'components/dashboard/LineChartCard';
 
-export default function Blockchain() {
+export default function Blockchain({
+  totalTransaction,
+  totalTransaction24,
+  totalWallets,
+  totalWallets24
+}: {
+  totalTransaction: string;
+  totalTransaction24: string;
+  totalWallets: string;
+  totalWallets24: string;
+}) {
   return (
     <MainCard>
       <Grid container spacing={2}>
@@ -15,16 +25,23 @@ export default function Blockchain() {
           <Typography variant="h3">Blockchain</Typography>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <AnalyticCard title="Total transaction" count="4,42,236" percentage={59.3} extra="35,000" />
+          <AnalyticCard title="Total transaction" count={totalTransaction} percentage={59.3} extra="35,000" />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <AnalyticCard title="Transactions in last 24 hours" count="78,250" percentage={70.5} extra="8,900" />
+          <AnalyticCard title="Transactions in last 24 hours" count={totalTransaction24} percentage={70.5} extra="8,900" />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <AnalyticCard title="Total Wallets" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
+          <AnalyticCard title="Total Wallets" count={totalWallets} percentage={27.4} isLoss color="warning" extra="1,943" />
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
-          <AnalyticCard title="New Wallets in last 24 hours" count="$35,078" percentage={27.4} isLoss color="warning" extra="$20,395" />
+          <AnalyticCard
+            title="New Wallets in last 24 hours"
+            count={totalWallets24}
+            percentage={27.4}
+            isLoss
+            color="warning"
+            extra="$20,395"
+          />
         </Grid>
         <Grid item xs={12} md={7} lg={8}>
           <LineChartCard title="Daily transactions" />
