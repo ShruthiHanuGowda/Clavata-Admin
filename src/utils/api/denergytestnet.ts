1f244079373a943cc69b003925286a77dbd47621
parth shahu <parth@d.energy>
2025-06-27 19:08:23 +0530

    New Wallets in last 24 hours

--- src/utils/api/denergytestnet.ts
+++ src/utils/api/denergytestnet.ts
@@ -17,6 +17,18 @@ export const getCharts = async () => {
   return data;
 };
 
+export const getNewAccounts = async () => {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const dd = String(today.getDate()).padStart(2, '0');
+
+  const formattedDate = `${yyyy}-${mm}-${dd}`;
+
+  const { data } = await api.get(`/stats-api/api/v1/lines/newAccounts?from=${formattedDate}&to=${formattedDate}&resolution=DAY`);
+  return data;
+};
+
 export const getChartDataById = async (id: string, slot: string) => {
   const { data } = await api.get(`/stats-api/api/v1/lines/${id}?resolution=${slot.toUpperCase()}`);
   return data;
